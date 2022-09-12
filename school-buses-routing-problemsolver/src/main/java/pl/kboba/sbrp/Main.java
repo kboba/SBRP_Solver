f93474c024471acdeba6baae1766f95f21288a1b
BobaK <boba.krzysztof98@gmail.com>
2022-09-13 01:37:51 +0200

    do_not_commit_now

--- school-buses-routing-problemsolver/src/main/java/pl/kboba/sbrp/Main.java
+++ school-buses-routing-problemsolver/src/main/java/pl/kboba/sbrp/Main.java
@@ -1,7 +1,16 @@
 package pl.kboba.sbrp;
 
+import pl.kboba.sbrp.model.City;
+import pl.kboba.sbrp.solver.BasicProblemPrinter;
+import pl.kboba.sbrp.solver.BasicProblemSolver;
+import pl.kboba.sbrp.solver.ProblemSolver;
+import pl.kboba.sbrp.solver.ResultPrinter;
+
 public class Main {
     public static void main(String[] args) {
-        // Initialize
+        City city = new City();
+        ProblemSolver basicProblemSolver = new BasicProblemSolver(city);
+        ResultPrinter resultPrinter = new BasicProblemPrinter(basicProblemSolver);
+        resultPrinter.printResult();
     }
 }
